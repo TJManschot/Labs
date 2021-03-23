0bf33da16efb3fe1940579793702939f4dad6c20
TJManschot <tj.manschot@belastingdienst.nl>
2021-03-23 15:52:45 +0100

    mockito en logging dependencies + gebruik van @InjectMocks

--- TDD2/src/test/java/nl/belastingdienst/opgave3/TrajectPrijsServiceTest.java
+++ TDD2/src/test/java/nl/belastingdienst/opgave3/TrajectPrijsServiceTest.java
@@ -3,19 +3,24 @@ package nl.belastingdienst.opgave3;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class TrajectPrijsServiceTest {
-    TrajectPrijsService trajectPrijsService = new TrajectPrijsService();
-
-    TrajectEenhedenNaarPrijsService mockedTrajectEenhedenNaarPrijsService = mock(TrajectEenhedenNaarPrijsService.class);
-    TrajectNaarTrajectEenhedenService mockedTrajectNaarTrajectEenhedenService = mock(TrajectNaarTrajectEenhedenService.class);
+    @Mock
+    TrajectEenhedenNaarPrijsService mockedTrajectEenhedenNaarPrijsService;
+    @Mock
+    TrajectNaarTrajectEenhedenService mockedTrajectNaarTrajectEenhedenService;
+    @InjectMocks
+    private TrajectPrijsService target;
 
     @BeforeEach
-    void init() throws Exception {
-        trajectPrijsService.setTrajectEenhedenNaarPrijsService(mockedTrajectEenhedenNaarPrijsService);
-        trajectPrijsService.setTrajectNaarTrajectEenhedenService(mockedTrajectNaarTrajectEenhedenService);
-
+    void init() {
         when(mockedTrajectEenhedenNaarPrijsService.getTrajectEenhedenPrijs(0)).thenReturn(10);
         when(mockedTrajectEenhedenNaarPrijsService.getTrajectEenhedenPrijs(-1)).thenThrow(new InvalidLocationException());
         when(mockedTrajectNaarTrajectEenhedenService.getTrajectEenheden("Zwolle", "Nijmegen")).thenReturn(0);
@@ -24,7 +29,7 @@ public class TrajectPrijsServiceTest {
 
     @Test
     void getTrajectPrijs() {
-        Assertions.assertEquals(10, trajectPrijsService.getTrajectPrijs("Zwolle", "Nijmegen"));
-        Assertions.assertThrows(InvalidLocationException.class, () -> trajectPrijsService.getTrajectPrijs("Zwolle", "Bielefeld"));
+        Assertions.assertEquals(10, target.getTrajectPrijs("Zwolle", "Nijmegen"));
+        Assertions.assertThrows(InvalidLocationException.class, () -> target.getTrajectPrijs("Zwolle", "Bielefeld"));
     }
 }
