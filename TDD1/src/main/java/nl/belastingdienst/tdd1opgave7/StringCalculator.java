0d306cfb1a8e722527d217fc0e8e67bcf9fde7ad
TJManschot <tj.manschot@belastingdienst.nl>
2021-03-29 18:10:36 +0200

    Verbiedt negatieve getallen en laat de eerste zien.

--- TDD1/src/main/java/nl/belastingdienst/tdd1opgave7/StringCalculator.java
+++ TDD1/src/main/java/nl/belastingdienst/tdd1opgave7/StringCalculator.java
@@ -3,10 +3,11 @@ package nl.belastingdienst.tdd1opgave7;
 import java.util.Arrays;
 
 public class StringCalculator {
-    public int add(String input) {
+    public int add(String input){
+        String delimiter = ",|\n";
         if (input.startsWith("//")) {
-            return Arrays.stream(input.split(input.substring(2,3))).map(String::strip).filter((String s) -> !s.equals("") && !s.contains("/")).mapToInt(Integer::parseInt).sum();
+            delimiter = input.substring(2,3);
         }
-        return Arrays.stream(input.split("[,\n]")).map(String::strip).filter((String s) -> !s.equals("")).mapToInt(Integer::parseInt).sum();
+        return Arrays.stream(input.split(delimiter)).map(String::strip).filter((String s) -> !s.equals("") && !s.contains("/")).filter((String s) -> { if(!s.contains("-")) return true; else throw new NumberFormatException("Negatives not allowed! You passed " + s + "."); }).mapToInt(Integer::parseInt).sum();
     }
 }
