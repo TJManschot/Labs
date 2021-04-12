c4e4008c91c534c4a37436cd097cbf5480b955b2
TJManschot <tj.manschot@belastingdienst.nl>
2021-04-12 15:47:17 +0200

    Lab 13 toegevoegd

---
+++ H13/src/test/java/h13opgave5/MyIntCollectionTest.java
@@ -0,0 +1,28 @@
+package h13opgave5;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyIntCollectionTest {
+    MyIntCollection<Integer> myIntCollection = new MyIntCollection<>();
+    private final int COLLECTION_SIZE = 17;
+
+    @BeforeEach
+    void init() {
+        for (int i = 0; i < COLLECTION_SIZE; i++) {
+            myIntCollection.add(100 - i);
+        }
+    }
+
+    @Test
+    void length() {
+        assertEquals(COLLECTION_SIZE, myIntCollection.length());
+    }
+
+    @Test
+    void printColelction() {
+        myIntCollection.printCollection();
+    }
+}
